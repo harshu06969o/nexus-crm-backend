8ddae8917d07084c5cfb8b8fdf171609831f7834
Harshu Chaturvedi <hchaube7@gmail.com>
2026-04-28 00:21:44 +0530

    Added CrossOrigin support

--- Type-sf-mini/src/main/java/com/crm/Type/sf_mini/controller/CustomerController.java
+++ Type-sf-mini/src/main/java/com/crm/Type/sf_mini/controller/CustomerController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin(origins = "*")
 
 @RestController
 @RequestMapping("/api/customers")
